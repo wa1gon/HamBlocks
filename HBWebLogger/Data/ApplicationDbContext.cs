94b400f8a04408e155b34cb1634223a0b5528b3e
Darryl Wagoner <darryl@wagoner.me>
2025-06-13 12:14:18 -0500

    add blazor with auth

---
+++ HBWebLogger/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HBWebLogger.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
