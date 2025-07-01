81a442c6481e79ac966dde5a16b107f31a59f469
Darryl Wagoner <darryl@wagoner.me>
2025-07-01 13:26:14 -0500

    added fast endpoints example

---
+++ FastEndpointServer/Responses/WeatherForecastResponse.cs
@@ -0,0 +1,9 @@
+namespace FastEndpointServer.Responses;
+
+public class WeatherForecastResponse
+{
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public int TemperatureF { get; set; }
+    public string Summary { get; set; }
+}; 
---
+++ FastEndpointServer/Validators/WeatherForecastRetrievalValidator.cs
@@ -0,0 +1,19 @@
+using FastEndpoints;
+using FluentValidation;
+
+namespace FastEndpointServer.Validators;
+
+public class WeatherForecastRetrievalValidator: Validator<WeatherForecastRequest>
+{
+    public WeatherForecastRetrievalValidator()
+    {
+
+        // Assuming Date is a property in the request, if not, you can remove this part.
+        // If Date is not part of the request, you can remove this rule.
+        RuleFor(x => x.Days)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Date must be greater than or equal to 1.")
+            .LessThanOrEqualTo(14)
+            .WithMessage("Date must be less than or equal to 14.");
+    }
+}
