e42735909735f561f7243ecf3506121b40987758
Darryl Wagoner <darryl@wagoner.me>
2025-07-02 11:16:14 -0500

    added examples

---
+++ HBLoggingService/Responses/Examples/WeatherForecastResponse.cs
@@ -0,0 +1,9 @@
+namespace HBLoggingService.Responses.Examples;
+
+public class WeatherForecastResponse
+{
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public int TemperatureF { get; set; }
+    public string Summary { get; set; }
+}; 
