177af6fe404f5b9a29d83d94778fb8d8d6539a64
Darryl Wagoner <darryl@wagoner.me>
2025-07-04 18:00:08 -0500

    file added

---
+++ ConsoleUpload/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var filePath = @"C:\temp\mycallcheck.adi";
+        var uploadUrl = "http://localhost:5000/upload";
+
+        using var client = new HttpClient()
+        {
+            Timeout = TimeSpan.FromMinutes(15) // debug
+        };
+        using var form = new MultipartFormDataContent();
+        using var fileStream = File.OpenRead(filePath);
+        var fileContent = new StreamContent(fileStream);
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+        // "File" must match the property name in your endpoint
+        form.Add(fileContent, "File", Path.GetFileName(filePath));
+
+        var response = await client.PostAsync(uploadUrl, form);
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine($"Status: {response.StatusCode}");
+        Console.WriteLine("Response:");
+        Console.WriteLine(responseContent);
+    }
+}
