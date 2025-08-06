f737fd0dbc5d749ba796f621c37697908f702665
Darryl Wagoner <darryl@wagoner.me>
2025-08-06 17:28:33 -0500

    Added inferfaces for models

---
+++ HBAbstractions/IQsoDetails.cs
@@ -0,0 +1,9 @@
+namespace HBAbstractions;
+public interface IQsoDetail
+{
+    int Id { get; set; }
+    Guid QsoId { get; set; }
+    string FieldName { get; set; }
+    string FieldValue { get; set; }
+    IQso? Qso { get; set; }
+}
---
+++ HBAbstractions/IQsoQslInfo.cs
@@ -0,0 +1,10 @@
+namespace HBAbstractions;
+public interface IQsoQslInfo
+{
+    int Id { get; set; }
+    string QslService { get; set; }
+    bool QslSent { get; set; }
+    bool QslReceived { get; set; }
+    Guid QsoId { get; set; }
+    IQso Qso { get; set; }
+}
