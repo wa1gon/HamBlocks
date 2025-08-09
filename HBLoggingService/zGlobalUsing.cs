b5208adab261e26a26454db7b70af5b15e505849
Darryl Wagoner <darryl@wagoner.me>
2025-08-09 15:41:35 -0500

    format change

--- HBLoggingService/zGlobalUsing.cs
+++ HBLoggingService/zGlobalUsing.cs
@@ -3,7 +3,7 @@ global using FastEndpoints;
 global using HamBlocks.Library.Models;
 global using HbLibrary.Adif; // Adjust namespace as needed
 global using HBLoggingService.Data;
-global using  HBLoggingService.Models;
+global using HBLoggingService.Models;
 global using HBLoggingService.Requests;
 global using HBLoggingService.Responses;
 global using Microsoft.EntityFrameworkCore;
