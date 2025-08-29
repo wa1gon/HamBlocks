88213af07577ffa1f851c6d8b75a806c131c10b7
Darryl Wagoner <darryl@wagoner.me>
2025-08-29 15:25:55 -0500

    working settings

--- LoggerWPF.Core/MainViewModel.cs
+++ LoggerWPF.Core/MainViewModel.cs
@@ -1,8 +1,18 @@
+using CommunityToolkit.Mvvm.Input;
+
 namespace LoggerWPF.Core;
 
 
 
 public partial class MainViewModel : ObservableObject
 {
-    
+    [RelayCommand]
+    public void SettingsMenuClick()
+    {
+        Console.WriteLine(nameof(SettingsMenuClickCommand));
+        // CurrentViewModel = _settingsViewModel;
+    }
+
+    public object CurrentViewModel { get; set; }
+
 }
--- LoggerWPF.View/MainWindow.xaml.cs
+++ LoggerWPF.View/MainWindow.xaml.cs
@@ -10,5 +10,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new LoggerWPF.Core.MainViewModel();
     }
 }
