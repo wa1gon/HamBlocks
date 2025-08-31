d6431c9e7bbabc31859c165404b1ab11e9acd599
Darryl Wagoner <darryl@wagoner.me>
2025-08-31 18:17:46 -0500

    wip

--- LoggerWPF.Views/App.xaml.cs
+++ LoggerWPF.Views/App.xaml.cs
@@ -32,7 +32,7 @@ namespace LoggerWPF
                 .ConfigureServices((ctx, services) =>
                 {
                     // Register all your services/VMS/windows ONCE here
-                    AddServices(services);
+                    AddServices(services, ctx.Configuration);
                 })
                 .Build();
             
@@ -45,14 +45,28 @@ namespace LoggerWPF
             mainWindow.Show();
         }
 
-        private static void AddServices(IServiceCollection services)
+        private static void AddServices(IServiceCollection services, IConfiguration configuration)
         {
-            services.AddHttpClient();
-            
             services.AddSingleton<IHbConfClientApiService, HbConfClientApiService>();
             services.AddSingleton<SettingsViewModel>();
             services.AddSingleton<MainViewModel>();
             services.AddSingleton<MainWindow>();
+            services.AddOptions<ApiOptions>()
+                .Bind(configuration.GetSection("Api"))
+                .Validate(o => Uri.IsWellFormedUriString(o.BaseUrl, UriKind.Absolute),
+                    "Api:BaseUrl must be an absolute URL")
+                .ValidateOnStart();
+            
+            // services.AddHttpClient();
+            services.AddHttpClient<IHbConfClientApiService, HbConfClientApiService>(
+                (sp, client) =>
+                {
+                    var opts = sp.GetRequiredService<IOptions<ApiOptions>>().Value;
+                    var baseUrl = opts.BaseUrl.EndsWith("/") ? opts.BaseUrl : opts.BaseUrl + "/";
+                    client.BaseAddress = new Uri(baseUrl);
+                    client.Timeout = TimeSpan.FromSeconds(15);
+                    client.DefaultRequestHeaders.UserAgent.ParseAdd("HBWebLogger/1.0");
+                });
         }
 
         protected override void OnExit(ExitEventArgs e)
