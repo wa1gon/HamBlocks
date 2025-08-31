61055b6d0339204286eb506e8044d4fef2003aeb
Darryl Wagoner <darryl@wagoner.me>
2025-08-31 12:17:11 -0500

    added packages

--- LoggerWPF.Views/ZGlobalUsing.cs
+++ LoggerWPF.Views/ZGlobalUsing.cs
@@ -3,6 +3,5 @@ global using System.Windows;
 global using LoggerWPF.Core;
 global using Microsoft.Extensions.DependencyInjection;
 global using HBWebLogger.Services.ApiClients;
-global using System.Collections.ObjectModel;
-global using HamBlocks.Library.Models;
-// global using HBWebLogger.Services.ApiClients;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
